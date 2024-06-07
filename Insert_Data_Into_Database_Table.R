80d89415e4333af7a77600a05b3ff12b81b313d6
Szilágyi Ádám <sz.adam1989@gmail.com>
2024-06-07 10:50:17 +0200

    Add files via upload

--- Insert_Data_Into_Database_Table.R
+++ Insert_Data_Into_Database_Table.R
@@ -1,5 +1,5 @@
 library("RODBC")
-channelOracle <- odbcDriverConnect(paste("DRIVER={Oracle in OraClient18Home1_32bit};DBQ=EMERALD.KSH.HU;UID=", Sys.getenv("userid"), ";PWD=", Sys.getenv("pwd")))
+channelOracle <- odbcDriverConnect(paste("DRIVER={Oracle in OraClient18Home1_32bit};DBQ=EMERALD.KSH.HU;UID=", Sys.getenv("userid"), ";PWD=", Sys.getenv("pwd")), DBMSencoding = "latin1")
 
 #1. tábla
 sqlSave(channel = channelOracle, dat = Q0102, tablename = "VB_REP.MNB_NAPI", append = TRUE, rownames = FALSE, colnames = FALSE, fast = FALSE)
